c2296099eb6e8b8cf45fa52e60c8ac0d4ba9d71d
Mike Lirio <michaelirio@gmail.com>
2021-03-24 20:33:14 +0000

    Case: adding a setup.sh file to simplify configuration.
    Some fix on name files.

---
+++ scripts/case/setup.sh
@@ -0,0 +1,26 @@
+echo "Setting up the scripts for the Nespi 4 Case."
+
+echo "Copying the scripts for the Restart button..."
+echo "Stopping the nespi4case.restart.service service..."
+systemctl stop nespi4case.restart.service
+systemctl disable nespi4case.restart.service
+echo "Copying service and script files..."
+cp ./nespi4case.restart.service /etc/systemd/system/nespi4case.restart.service
+cp ./nespi4case.restart.gpio.py /opt/delirio/case/nespi4case.restart.gpio.py
+
+echo "Copying the scripts for the Shutdown button..."
+echo "Stopping the nespi4case.shutdown.service service..."
+systemctl stop nespi4case.shutdown.service
+systemctl disable nespi4case.shutdown.service
+echo "Copying service and script files..."
+cp ./nespi4case.shutdown.service /etc/systemd/system/nespi4case.shutdown.service
+cp ./nespi4case.shutdown.gpio.py /opt/delirio/case/nespi4case.shutdown.gpio.py
+
+echo "Copying the safe shutdown script"
+cp ./safe.close.sh /opt/delirio/case/safe.close.sh 
+
+echo "Loading the new daemons"
+systemctl daemon-reload
+systemctl reset-failed
+
+echo "Setting up finnished."
