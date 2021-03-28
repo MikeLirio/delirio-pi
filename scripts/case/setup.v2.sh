8db6f67f4ad57a3f9c70949d6e958262da58dafd
Mike Lirio <michaelirio@gmail.com>
2021-03-28 00:56:00 +0000

    Revision of the setup.v2.sh script.

--- scripts/case/setup.v2.sh
+++ scripts/case/setup.v2.sh
@@ -1,63 +1,63 @@
 #!/bin/bash
 
-#########################################################################
-# DESCRIPTION ###########################################################
-#########################################################################
-#                                                                       #
-#   Version 0.1                                                         #
-#                                                                       #
-#   This job will set the script & service files to control the buttons #
-#   of the Raspberry Pi.                                                #
-#                                                                       #
-#   Flags:                                                              #
-#       -r : set the files for the reset button.                        #
-#       -s : set the files for the shutdown button.                     #
-#       -a : set the files for the reset & shutdown buttons.            #
-#       -h : print in console this comments.                            #
-#                                                                       #
-#########################################################################
-# IMPORTED SCRIPTS ######################################################
-#########################################################################
+###############################################################################################
+# DESCRIPTION #################################################################################
+###############################################################################################
+#                                                                                             #
+#   Version 0.1                                                                               #
+#                                                                                             #
+#   This job will set the script & service files to control the buttons of the Raspberry Pi.  #
+#                                                                                             #
+#   Flags:                                                                                    #
+#       -r / --reset : set the files for the reset button.                                    #
+#       -s / --shutdown : set the files for the shutdown button.                              #
+#       -a / --all : set the files for the reset & shutdown buttons.                          #
+#       -h / --help : print in console this comments.                                         #
+#                                                                                             #
+###############################################################################################
+# IMPORTED SCRIPTS ############################################################################
+###############################################################################################
 
 . ./../colors.sh        # Variables with the colors for the terminal.
 . ./../progress_bar.sh  # Progress bar.
 
-#########################################################################
-# VARIABLES #############################################################
-#########################################################################
+###############################################################################################
+# VARIABLES ###################################################################################
+###############################################################################################
 
-#########################################################################
+RP_SCRIPT_CASE_PATH=/opt/delirio/case   # Path to move the CASE scripts.
+UBUNTU_SYSTEMD=/etc/systemd/system      # Path to move the new services for the buttons.
+
+###############################################################################################
 
 function restart() {
     echo -e "${GRE}#::setup.sh::# ${YEL}Copying the scripts for the Restart button...${NC}"
     echo -e "${GRE}#::setup.sh::# ${YEL}Stopping the nespi4case.restart.service service...${NC}"
 
-    #systemctl stop nespi4case.restart.service
-    #systemctl disable nespi4case.restart.service
+    # systemctl stop nespi4case.restart.service
+    # systemctl disable nespi4case.restart.service
 
     echo -e "${GRE}#::setup.sh::# ${YEL}Copying service and script files...${NC}"
 
-    #cp ./nespi4case.restart.service /etc/systemd/system/nespi4case.restart.service
-    #cp ./nespi4case.restart.gpio.py /opt/delirio/case/nespi4case.restart.gpio.py
+    # cp ./nespi4case.restart.service $UBUNTU_SYSTEMD/nespi4case.restart.service
+    # cp ./nespi4case.restart.gpio.py $RP_SCRIPT_CASE_PATH/nespi4case.restart.gpio.py
 
     echo -e "${GRE}#::setup.sh::# ${YEL}Enabling the nespi4case.restart.service service...${NC}"
 
-    #systemctl enable nespi4case.restart.service
-
-    echo -ne '#############             (66%)\r'
+    # systemctl enable nespi4case.restart.service
 }
 
 function shutdown() {
     echo -e "${GRE}#::setup.sh::# ${YEL}Copying the scripts for the Shutdown button...${NC}"
     echo -e "${GRE}#::setup.sh::# ${YEL}Stopping the nespi4case.shutdown.service service...${NC}"
 
-    #systemctl stop nespi4case.shutdown.service
-    #systemctl disable nespi4case.shutdown.service
+    # systemctl stop nespi4case.shutdown.service
+    # systemctl disable nespi4case.shutdown.service
 
     echo -e "${GRE}#::setup.sh::# ${YEL}Copying service and script files...${NC}"
 
-    # cp ./nespi4case.shutdown.service /etc/systemd/system/nespi4case.shutdown.service
-    # cp ./nespi4case.shutdown.gpio.py /opt/delirio/case/nespi4case.shutdown.gpio.py
+    # cp ./nespi4case.shutdown.service $UBUNTU_SYSTEMD/nespi4case.shutdown.service
+    # cp ./nespi4case.shutdown.gpio.py $RP_SCRIPT_CASE_PATH/nespi4case.shutdown.gpio.py
 
     echo -e "${GRE}#::setup.sh::# ${YEL}Enabling the nespi4case.shutdown.service service...${NC}"
 
@@ -65,7 +65,7 @@ function shutdown() {
 
     echo -e "${GRE}#::setup.sh::# ${YEL}Copying the safe shutdown script${NC}"
 
-    # cp ./safe.close.sh /opt/delirio/case/safe.close.sh 
+    # cp ./safe.close.sh $RP_SCRIPT_CASE_PATH/safe.close.sh 
 }
 
 function apply_daemons() {
@@ -75,11 +75,10 @@ function apply_daemons() {
     # systemctl reset-failed
 }
 
-
 echo -e "${GRE}#::setup.sh::# ${YEL}Setting up the scripts for the Nespi 4 Case.${NC}"
 
 restart
 shutdown
 apply_daemons
 
-echo -e "${GRE}#::setup.sh::# ${YEL}Setting up finnished.${NC}"
+echo -e "${GRE}#::setup.sh::# ${YEL}Setting up finished.${NC}"
