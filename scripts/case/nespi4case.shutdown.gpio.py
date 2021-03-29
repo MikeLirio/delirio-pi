0346deb6049dd776be8d1903cd0da6497efeedc8
Mike Lirio <michaelirio@gmail.com>
2021-03-29 22:38:47 +0100

    case: shutdown button test.

--- scripts/case/nespi4case.shutdown.gpio.py
+++ scripts/case/nespi4case.shutdown.gpio.py
@@ -1,27 +1,31 @@
 try:
     import RPi.GPIO as GPIO
+    from multiprocessing import Process
     import os
 
     # Pins of the Raspberry Pi
     shutdownPin = 5
 
-    def button_shutdown(channel):
+    def init():
+        # Use physical pin numbering
+        GPIO.setmode(GPIO.BOARD) 
+        # Set pin 5 to be an input pin and set initial value to be pulled low (off)
+        GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+
+    def shutdownButton():
+        GPIO.wait_for_edge(shutdownPin, GPIO.RAISING)
         print("Shutting down")
         os.system("bash /opt/delirio/case/safe.close.sh")
         #os.system("sudo shutdown -h now")
 
-    # Use physical pin numbering
-    GPIO.setmode(GPIO.BOARD) 
-    # Set pin 5 to be an input pin and set initial value to be pulled low (off)
-    GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-    # Setup event on pin 5 rising edge
-    GPIO.wait_for_edge(shutdownPin,GPIO.FALLING) 
-    
-    GPIO.cleanup() # Clean up
+    if __name__ == "__main__":
+        init()
 
-    print("Shutting down")
-    os.system("bash /opt/delirio/case/safe.close.sh")
-    #os.system("sudo shutdown -h now")
+        shutdownProcess = Process(target = shutdownButton)
+        shutdownProcess.start()
+        shutdownProcess.join()
+
+        GPIO.cleanup()
 
 except RuntimeError as error:
     print("Error importing RPi.GPIO! \n This is probably because you need superuser privileges. \n You can achieve this by using 'sudo' to run your script")
