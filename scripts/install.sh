4cb6359d616329103794c24abe59d8318b175a5e
Mike Lirio <michaelirio@gmail.com>
2021-05-19 19:38:55 +0100

    Scripts | Deletion of duplicated files and rename.

--- scripts/install.sh
+++ scripts/install.sh
@@ -24,37 +24,37 @@ install_log="${GRE}#::${YEL}./install.sh${GRE}::#${NC}"
 function initial_setup() {
     echo -e "$install_log ${YEL} List of important environment variables to be defined on the system:${NC}"
     #if [[ $BASE_PATH_SCRIPTS = "."]]; then
-    #    echo -e "${install_log}${YEL} Setting the ${BYEL}Delirio Pi${YEL} configuration. Thanks for downloading!${NC}"
+    #    echo -e "$install_log${YEL} Setting the ${BYEL}Delirio Pi${YEL} configuration. Thanks for downloading!${NC}"
     #fi
 }
 
 function bash_scripts_setup() {
-    echo -e "${install_log}${YEL} bash_scripts_setup${NC}"
+    echo -e "$install_log${YEL} bash_scripts_setup${NC}"
 }
 
 function docker_setup() {
-    echo -e "${install_log}${YEL} docker_setup${NC}"
+    echo -e "$install_log${YEL} docker_setup${NC}"
 }
 
 function unistall() {
-    echo -e "${install_log}${YEL} unistall${NC}"
+    echo -e "$install_log${YEL} unistall${NC}"
 }
 
 function bash_profiles() {
-    echo -e "${install_log}${YEL} bash_profiles${NC}"
+    echo -e "$install_log${YEL} bash_profiles${NC}"
 }
 
 while true; do
-    echo -e "$install_log ${YEL} Setting the ${BYEL}Delirio Pi${YEL} configuration. Thanks for downloading!${NC}"
-    echo -e "$install_log ${YEL} What to you want to setup? ${NC}"
-    echo -e "$install_log ${BLU}    [${RED}0${BLU}] Initial setup.${NC}"
-    echo -e "$install_log ${BLU}    [${RED}1${BLU}] Move sh scripts.${NC}"
-    echo -e "$install_log ${BLU}    [${RED}2${BLU}] Docker.${NC}"
-    echo -e "$install_log ${BLU}    [${RED}3${BLU}] Install/Update .bash_profile files.${NC}"
-    echo -e "$install_log ${BLU}    [${RED}4${BLU}] Uninstall.${NC}"
-    echo -e "$install_log ${BLU}    [${RED}5${BLU}] Exit.${NC}"
+    echo -e "$install_log${YEL} Setting the ${BYEL}Delirio Pi${YEL} configuration. Thanks for downloading!${NC}"
+    echo -e "$install_log${YEL} What to you want to setup? ${NC}"
+    echo -e "$install_log${BLU}    [${RED}0${BLU}] Initial setup.${NC}"
+    echo -e "$install_log${BLU}    [${RED}1${BLU}] Move sh scripts.${NC}"
+    echo -e "$install_log${BLU}    [${RED}2${BLU}] Docker.${NC}"
+    echo -e "$install_log${BLU}    [${RED}3${BLU}] Install/Update .bash_profile files.${NC}"
+    echo -e "$install_log${BLU}    [${RED}4${BLU}] Uninstall.${NC}"
+    echo -e "$install_log${BLU}    [${RED}5${BLU}] Exit.${NC}"
 
-    read -p "$(echo -e $install_log ${YEL} Write the number of the option: ${NC})" option
+    read -p "$(echo -e $install_log${YEL} Write the number of the option: ${NC})" option
     case $option in 
         0 ) environment_variable_setup;;
         1 ) bash_scripts_setup;;
@@ -62,8 +62,8 @@ while true; do
         3 ) bash_profiles;;
         4 ) unistall;;
         5 ) break;;
-        * ) echo -e "$install_log ${RED} No valid option. Try again...${NC}";;
+        * ) echo -e "$install_log${RED} No valid option. Try again...${NC}";;
     esac
 done
 
-echo -e "$install_log ${YEL} Thanks for using ${BYEL}Delirio Pi${YEL} scripts.${NC}"
+echo -e "$install_log${YEL} Thanks for using ${BYEL}Delirio Pi${YEL} scripts.${NC}"
