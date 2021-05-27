96cd2a427d0fb3f31a3d73417eafbcf66c2d6d6e
Mike Lirio <michaelirio@gmail.com>
2021-05-27 15:17:33 +0100

    Scripts | Added main as a default branch.

--- scripts/src/config/default.sh
+++ scripts/src/config/default.sh
@@ -4,5 +4,5 @@ export CONF_FILE="$BASE_PATH_SCRIPTS/scripts/config/default.conf"
 
 export GITHUB_PATH="/opt/github"
 export GITHUB_DELIRIO="$GITHUB_PATH/delirio-pi"
-export GITHUB_DELIRIO_BRANCH="feature/docker/nginx"
+export GITHUB_DELIRIO_BRANCH="main"
 export GITHUB_DELIRIO_URL="https://github.com/MikeLirio/delirio-pi.git"
