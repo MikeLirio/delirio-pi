b56a9e99a21ffba7a3f8ba844672f721e03720d8
Mike Lirio <michaelirio@gmail.com>
2021-05-27 11:53:05 +0100

    Script | Profile | Color change and control if /home directory is empty

--- scripts/src/utils/utils.sh
+++ scripts/src/utils/utils.sh
@@ -20,29 +20,29 @@ fi
 
 export UTILS_SH_IMPORTED=true;
 
-utils_log="${GRE}#::${YEL}./utils/utils.sh${GRE}::#${NC}"
+utils_log="${GRE}#::${BYEL}./utils/utils.sh${GRE}::#${NC}"
 
 ###############################################################################################
 
 function read_configuration() {
     while true; do
-        echo -e "${utils_log}${YEL} Making a backup of file /etc/bash.bashrc ${NC}"
+        echo -e "${utils_log}${BYEL} Making a backup of file /etc/bash.bashrc ${NC}"
         cp /etc/bash.bashrc $BASE_PATH_SCRIPTS/config/bash.bashrc.backup
-        echo -e "${utils_log}${YEL} Do you want to import the default configuration file to the system?${NC}"
+        echo -e "${utils_log}${BYEL} Do you want to import the default configuration file to the system?${NC}"
         echo "$(cat $BASE_PATH_SCRIPTS/config/default.sh)"
 
-        read -p "$(echo -e ${utils_log}${YEL} Y/N: ${NC})" option
+        read -p "$(echo -e ${utils_log}${BYEL} Y/N: ${NC})" option
         case $option in 
             Y | y )
-                echo -e "${utils_log}${YEL} Loading ${BYEL}default${YEL} configuration...${NC}"
+                echo -e "${utils_log}${BYEL} Loading ${BYEL}default${BYEL} configuration...${NC}"
                 cp $BASE_PATH_SCRIPTS/config/default.sh /etc/profile.d/delirio.env.sh
                 echo ". /etc/profile.d/delirio.env.sh" >> /etc/bash.bashrc
                 post_configuration_done
                 break
                 ;;
             N | n) 
-                read -p "$(echo -e ${utils_log}${YEL} Write the fullpath with the filename to read it. (Ex. /opt/config/custom.conf))" custom
-                echo -e "${utils_log}${YEL} Loading $custom configuration...${NC}"
+                read -p "$(echo -e ${utils_log}${BYEL} Write the fullpath with the filename to read it. (Ex. /opt/config/custom.conf))" custom
+                echo -e "${utils_log}${BYEL} Loading $custom configuration...${NC}"
                 cp $custom /etc/profile.d/delirio.env.sh
                 post_configuration_done
                 break
@@ -61,53 +61,56 @@ function post_configuration_done() {
 }
 
 function move_delirio_system_files() {
-    echo -e "${utils_log}${YEL} Creating folder $BASE_PATH_SCRIPTS .${NC}"
+    echo -e "${utils_log}${BYEL} Creating folder $BASE_PATH_SCRIPTS .${NC}"
     mkdir -p $BASE_PATH_SCRIPTS/scripts
-    echo -e "${utils_log}${YEL} Setting privileges 777 on /opt RPi folders.${NC}"
+    echo -e "${utils_log}${BYEL} Setting privileges 777 on /opt RPi folders.${NC}"
     chown -R 777 $BASE_PATH_SCRIPTS/scripts
-    echo -e "${utils_log}${YEL} Copying scripts $GITHUB_DELIRIO/scripts/src/utils to $BASE_PATH_SCRIPTS ${NC}"
+    echo -e "${utils_log}${BYEL} Copying scripts $GITHUB_DELIRIO/scripts/src/utils to $BASE_PATH_SCRIPTS ${NC}"
     cp -r $GITHUB_DELIRIO/scripts/src/utils $BASE_PATH_SCRIPTS/
-    echo -e "${utils_log}${YEL} Copying scripts $GITHUB_DELIRIO/scripts/src/config to $BASE_PATH_SCRIPTS ${NC}"
+    echo -e "${utils_log}${BYEL} Copying scripts $GITHUB_DELIRIO/scripts/src/config to $BASE_PATH_SCRIPTS ${NC}"
     cp -r $GITHUB_DELIRIO/scripts/src/config $BASE_PATH_SCRIPTS/
-    echo -e "${utils_log}${YEL} Copying scripts $GITHUB_DELIRIO/scripts/src/install.sh to $BASE_PATH_SCRIPTS ${NC}"
+    echo -e "${utils_log}${BYEL} Copying scripts $GITHUB_DELIRIO/scripts/src/install.sh to $BASE_PATH_SCRIPTS ${NC}"
     cp -r $GITHUB_DELIRIO/scripts/src/install.sh $BASE_PATH_SCRIPTS/
 }
 
 function move_nespi4case_system_files() {
-    echo -e "${utils_log}${YEL} Copying scripts $GITHUB_DELIRIO/scripts/src/case to $BASE_PATH_SCRIPTS ${NC}"
+    echo -e "${utils_log}${BYEL} Copying scripts $GITHUB_DELIRIO/scripts/src/case to $BASE_PATH_SCRIPTS ${NC}"
     cp -r $GITHUB_DELIRIO/scripts/src/case $BASE_PATH_SCRIPTS/
 }
 
 function remove_nespi4case_system_files() {
-    echo -e "${utils_log}${YEL} Removing files from $BASE_PATH_SCRIPTS/case ${NC}"
+    echo -e "${utils_log}${BYEL} Removing files from $BASE_PATH_SCRIPTS/case ${NC}"
     rm -rf $BASE_PATH_SCRIPTS/case
 }
 
 function unistall_initial_setup() {
-    echo -e "${utils_log}${YEL} Removing $BASE_PATH_SCRIPTS/install.sh file.${NC}"
+    echo -e "${utils_log}${BYEL} Removing $BASE_PATH_SCRIPTS/install.sh file.${NC}"
     rm $BASE_PATH_SCRIPTS/install.sh
-    echo -e "${utils_log}${YEL} Removing $BASE_PATH_SCRIPTS/utils file.${NC}"
+    echo -e "${utils_log}${BYEL} Removing $BASE_PATH_SCRIPTS/utils file.${NC}"
     rm -rf $BASE_PATH_SCRIPTS/utils
-    echo -e "${utils_log}${YEL} Removing $BASE_PATH_SCRIPTS/config file.${NC}"
+    echo -e "${utils_log}${BYEL} Removing $BASE_PATH_SCRIPTS/config file.${NC}"
     rm -rf $BASE_PATH_SCRIPTS/config
-    echo -e "${utils_log}${YEL} Removing /etc/profile.d/delirio.env.sh file.${NC}"
+    echo -e "${utils_log}${BYEL} Removing /etc/profile.d/delirio.env.sh file.${NC}"
     rm /etc/profile.d/delirio.env.sh
-    echo -e "${utils_log}${YEL} Loading backup of /etc/bash.bashrc${NC}"
+    echo -e "${utils_log}${BYEL} Loading backup of /etc/bash.bashrc${NC}"
     cp $BASE_PATH_SCRIPTS/config/bash.bashrc.backup /etc/bash.bashrc
-    echo -e "${utils_log}${YEL} Reloading the default profile.${NC}"
+    echo -e "${utils_log}${BYEL} Reloading the default profile.${NC}"
     source /etc/profile
     printenv
 }
 
 function install_bash_profiles() {
     if [[ -z "${GITHUB_DELIRIO}" ]]; then
-        echo -e "$install_log${YEL} Unable to import the .bash_profile files.${NC}"
-        echo -e "$install_log${YEL} Make sure that the environment variables are install from the step ${RED}[0] Initial Setup.${NC}"
+        echo -e "$install_log${BYEL} Unable to import the .bash_profile files.${NC}"
+        echo -e "$install_log${BYEL} Make sure that the environment variables are install from the step ${RED}[0] Initial Setup.${NC}"
+    elif [ -z "$(ls -A /home)" ]; then
+        echo -e "$install_log${BYEL} Unable to import the .bash_profile files.${NC}"
+        echo -e "$install_log${BYEL} The folder /home is empty and doesn not have any user. It could happen if you are running this script on ${CYA}Docker${BYEL}.${NC}"
     else
         actualFolder=$(pwd)
         cd /home
         for folder in */ ; do
-            echo -e "$install_log${YEL} Copying the file to the user $folder on /home/$folder ...${NC}"
+            echo -e "$install_log${BYEL} Copying the file to the user $folder on /home/$folder ...${NC}"
             cp $GITHUB_DELIRIO/scripts/src/profiles/bash_profile_template /home/$folder/.bash_profile
         done
         cd $actualFolder
@@ -119,7 +122,7 @@ function uninstall_bash_profiles() {
     actualFolder=$(pwd)
     cd /home
     for folder in */ ; do
-        echo -e "$install_log${YEL} Removing the file to the user $folder on /home/$folder ...${NC}"
+        echo -e "$install_log${BYEL} Removing the file to the user $folder on /home/$folder ...${NC}"
         rm /home/$folder/.bash_profile
     done
     cd $actualFolder
@@ -128,25 +131,25 @@ function uninstall_bash_profiles() {
 
 function printenvpi() {
     echo -e "${GRE} ######################################################################################################${NC}"
-    echo -e "${GRE} ## ${YEL}DELIRIO ENVIRONMENT VARIABLE ${GRE}#######################################################################${NC}"
+    echo -e "${GRE} ## ${BYEL}DELIRIO ENVIRONMENT VARIABLE ${GRE}#######################################################################${NC}"
     echo -e "${GRE} ######################################################################################################${NC}"
     echo -e " ${NC}"
-    echo -e " ${YEL}The .bash_profiles contain the next environment variables:  ${NC}"
+    echo -e " ${BYEL}The .bash_profiles contain the next environment variables:  ${NC}"
     echo -e " ${NC}"
-    echo -e "     ${BLU}VERSION                        ${RED}| ${YEL} $VERSION  ${NC}"
-    echo -e "     ${BLU}INSTALL_VERSION                ${RED}| ${YEL} $INSTALL_VERSION  ${NC}"
+    echo -e "     ${BLU}VERSION                        ${RED}| ${BYEL} $VERSION  ${NC}"
+    echo -e "     ${BLU}INSTALL_VERSION                ${RED}| ${BYEL} $INSTALL_VERSION  ${NC}"
     echo -e " ${NC}"
-    echo -e "     ${BLU}BASE_PATH_SCRIPTS              ${RED}| ${YEL} $BASE_PATH_SCRIPTS ${NC}"
-    echo -e "     ${BLU}CONFIG_FILE                    ${RED}| ${YEL} $CONFIG_FILE ${NC}"
+    echo -e "     ${BLU}BASE_PATH_SCRIPTS              ${RED}| ${BYEL} $BASE_PATH_SCRIPTS ${NC}"
+    echo -e "     ${BLU}CONFIG_FILE                    ${RED}| ${BYEL} $CONFIG_FILE ${NC}"
     echo -e " ${NC}"
-    echo -e "     ${BLU}DEBUG                          ${RED}| ${YEL} $DEBUG ${NC}"
+    echo -e "     ${BLU}DEBUG                          ${RED}| ${BYEL} $DEBUG ${NC}"
     echo -e " ${NC}"
-    echo -e "     ${BLU}GITHUB_PATH                    ${RED}| ${YEL} $GITHUB_PATH ${NC}"
-    echo -e "     ${BLU}GITHUB_DELIRIO                 ${RED}| ${YEL} $GITHUB_DELIRIO ${NC}"
-    echo -e "     ${BLU}GITHUB_DELIRIO_BRANCH          ${RED}| ${YEL} $GITHUB_DELIRIO_BRANCH ${NC}"
-    echo -e "     ${BLU}GITHUB_DELIRIO_URL             ${RED}| ${YEL} $GITHUB_DELIRIO_URL ${NC}"
+    echo -e "     ${BLU}GITHUB_PATH                    ${RED}| ${BYEL} $GITHUB_PATH ${NC}"
+    echo -e "     ${BLU}GITHUB_DELIRIO                 ${RED}| ${BYEL} $GITHUB_DELIRIO ${NC}"
+    echo -e "     ${BLU}GITHUB_DELIRIO_BRANCH          ${RED}| ${BYEL} $GITHUB_DELIRIO_BRANCH ${NC}"
+    echo -e "     ${BLU}GITHUB_DELIRIO_URL             ${RED}| ${BYEL} $GITHUB_DELIRIO_URL ${NC}"
     echo -e " ${NC}"
-    echo -e "     ${BLU}DOCKER_VOLUME_NAME_NGINX       ${RED}| ${YEL} $DOCKER_VOLUME_NAME_NGINX ${NC}"
+    echo -e "     ${BLU}DOCKER_VOLUME_NAME_NGINX       ${RED}| ${BYEL} $DOCKER_VOLUME_NAME_NGINX ${NC}"
     echo -e " ${NC}"
     echo -e "${GRE} ######################################################################################################${NC}"
 }
