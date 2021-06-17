4cd8ece0b2d9fbfe3d5166b6e902efda95a4a056
Mike Lirio <michaelirio@gmail.com>
2021-06-17 13:09:06 +0100

    Docker | Nginx | added new environment variables 4 the use of deliriopi

--- scripts/src/config/default.sh
+++ scripts/src/config/default.sh
@@ -6,3 +6,6 @@ export GITHUB_PATH="/opt/github"
 export GITHUB_DELIRIO="$GITHUB_PATH/delirio-pi"
 export GITHUB_DELIRIO_BRANCH="main"
 export GITHUB_DELIRIO_URL="https://github.com/MikeLirio/delirio-pi.git"
+
+export DELIRIO_HOST=192.168.1.94
+export DELIRIO_TIMEZONE="Europe/London"
